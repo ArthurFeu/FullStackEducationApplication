aa79ba126ee97bd762090e3222c833b5544ce8d2
Arthur Feu <arthurfeufeu@gmail.com>
2024-07-27 20:19:38 -0300

    API DONE -> school route included

--- src/connections/database/server.js
+++ src/connections/database/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import studentsRouter from '../routes/students.js';
 import coursesRouter from '../routes/courses.js';
 import enrollmentsRouter from '../routes/enrollments.js';
+import schoolsRouter from '../routes/schools.js';
 
 const app = express();
 const PORT = 5000;
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use('/students', studentsRouter);
 app.use('/courses', coursesRouter);
 app.use('/enrollments', enrollmentsRouter);
+app.use('/schools', schoolsRouter);
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
