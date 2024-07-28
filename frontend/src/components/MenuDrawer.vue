2b799236377d3c109a2a1a2cc8a1390c4dec9348
Arthur Feu <arthurfeufeu@gmail.com>
2024-07-28 17:20:48 -0300

    CREATED menu component and first pages

---
+++ frontend/src/components/MenuDrawer.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-app-bar color="primary" elevation="0">
+		<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+		<v-toolbar-title>Grupo A</v-toolbar-title>
+	</v-app-bar>
+  
+	<v-navigation-drawer v-model="drawer" color="primary" class="align-center">
+		<v-list-item link title="Home" to="/"></v-list-item>
+		<v-list-item link title="Alunos" to="/students"></v-list-item>
+		<v-list-item link title="Cursos"></v-list-item>
+		<v-list-item link title="Escolas"></v-list-item>
+	</v-navigation-drawer>
+</template>
+
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	name: 'MenuDrawer',
+	props: {},
+	setup() {
+		const drawer = ref(false);
+		return {
+			drawer
+		};
+	}
+}
+</script>
---
+++ frontend/src/pages/students.vue
@@ -0,0 +1,18 @@
+<template>
+	<v-app>
+	<MenuDrawer />
+  
+	<v-main>
+		<v-container>
+		<div class="d-flex justify-space-between w-screen h-screen border rounded-lg">
+		</div>
+		</v-container>
+	</v-main>
+  
+	</v-app>
+  </template>
+  
+<script setup>
+  import MenuDrawer from '@/components/MenuDrawer.vue';
+</script>
+  
