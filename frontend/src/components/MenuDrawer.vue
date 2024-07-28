0a9554c413424f51bac4bbddf947c1523e40487d
Arthur Feu <arthurfeufeu@gmail.com>
2024-07-28 18:41:58 -0300

    UPDATE MenuDrawer AND CREATED Students page

--- frontend/src/components/MenuDrawer.vue
+++ frontend/src/components/MenuDrawer.vue
@@ -1,14 +1,21 @@
 <template>
-	<v-app-bar color="primary" elevation="0">
+	<v-app-bar color="secondary" elevation="0" app>
+      <v-img
+        src="../assets/logo.png"
+        contain
+      ></v-img>
+    </v-app-bar>
+
+	<v-app-bar color="primary" elevation="0" style="pagging-top">
 		<v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-		<v-toolbar-title>Grupo A</v-toolbar-title>
+		<v-toolbar-title @click="drawer = !drawer">Módulo Acadêmico</v-toolbar-title>
 	</v-app-bar>
   
 	<v-navigation-drawer v-model="drawer" color="primary" class="align-center">
-		<v-list-item link title="Home" to="/"></v-list-item>
-		<v-list-item link title="Alunos" to="/students"></v-list-item>
-		<v-list-item link title="Cursos"></v-list-item>
-		<v-list-item link title="Escolas"></v-list-item>
+		<v-list-item prepend-icon="mdi-home" link title="Home" to="/"></v-list-item>
+		<v-list-item prepend-icon="mdi-account-school" link title="Alunos" to="/students"></v-list-item>
+		<v-list-item prepend-icon="mdi-school" link title="Cursos"></v-list-item>
+		<v-list-item prepend-icon="mdi-town-hall" link title="Escolas"></v-list-item>
 	</v-navigation-drawer>
 </template>
 
--- frontend/src/pages/students.vue
+++ frontend/src/pages/students.vue
@@ -1,11 +1,45 @@
 <template>
 	<v-app>
 	<MenuDrawer />
-  
+	<br>
 	<v-main>
 		<v-container>
-		<div class="d-flex justify-space-between w-screen h-screen border rounded-lg">
-		</div>
+			<div class="justify-center w-screen h-screen ">
+				<div class="d-flex justify-space-between">
+					<!-- SEARCH BAR -->
+					<div class="d-flex justify-center w-75">
+						<v-text-field label="Pesquisar Alunos" variant="outlined"></v-text-field>
+					</div>
+					<!-- NEW STUDENT BUTTON -->
+					<div class="">
+						<v-btn color="primary" dark class="h-75">Cadastrar Aluno</v-btn>
+					</div>
+				</div>
+				<br>
+				<!-- RECORDS TABLE -->
+				<div class="d-flex justify-center w-auto h-auto border rounded-lg">
+					<v-table>
+					<tbody style="text-align: center;">
+							<tr style="font-weight: bold;">
+								<td>Registro Acadêmico</td>
+								<td>Nome</td>
+								<td>CPF</td>
+								<td>Ações</td>
+							</tr>
+							<tr>
+								<td>21.1.8017</td>
+								<td>Arthur Mendonça Feu</td>
+								<td>123.456.789-10</td>
+								<td>
+									<v-btn color="primary" text>Editar</v-btn>
+									&nbsp; <!-- Add a small space here -->
+									<v-btn color="error" text>Excluir</v-btn>
+								</td>
+							</tr>
+					</tbody>
+					</v-table>
+				</div>
+			</div>
 		</v-container>
 	</v-main>
   
